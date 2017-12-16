1bccc6d3ccfbd659e6a2d60d5558c4b1febbc14e
Sydney Erickson <sydneyerickson@icloud.com>
2017-12-15 23:56:33 -0800

    Fix builds for CMake... somehow.

--- scripts/buildlib.py
+++ scripts/buildlib.py
@@ -11,10 +11,10 @@ import buildsettings
 def execute_shell(command):
 	print("[EXECUTION] " + command)
 	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	for line in p.stdout.readlines():
+		print(line.decode("utf-8"), end='')
 	retval = p.wait()
 	if retval != 0:
-		for line in p.stdout.readlines():
-			print(line.decode("utf-8"), end='')
 		exit(-1)
 
 def build_configure(archive_name, configure_extra_args, custom_prefix=None):
