fd9dc009376b34c8852ec9bf2b649f2cbe2acf4b
Sydney Erickson <sydneyerickson@icloud.com>
2017-12-19 21:13:19 -0800

    Fix Mercurial build

--- scripts/build_mercurial.py
+++ scripts/build_mercurial.py
@@ -1,5 +1,12 @@
 # Copyright (c) Sydney Erickson 2017
+import os
+
 import buildlib
 import buildsettings
 
-buildlib.build_configure("mercurial-4.4.2.tar.gz", "")
+os.chdir(buildsettings.buildbase + "/pkg-src")
+
+buildlib.execute_shell("tar -xvf mercurial-4.4.2.tar.gz")
+os.chdir("mercurial-4.4.2")	
+buildlib.execute_shell("make local")
+buildlib.execute_shell("mv ./hg {}/bin".format(buildsettings.builddir))
