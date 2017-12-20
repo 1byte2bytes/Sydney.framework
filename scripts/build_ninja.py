ec8a20445007f275452811caf1440e401602edbd
Sydney Erickson <sydneyerickson@icloud.com>
2017-12-19 18:57:27 -0800

    Fix Ninja build

--- scripts/build_ninja.py
+++ scripts/build_ninja.py
@@ -1,5 +1,12 @@
 # Copyright (c) Sydney Erickson 2017
+import os
+
 import buildlib
 import buildsettings
 
-buildlib.build_configure("ninja-1.8.2.tar.gz", "")
+os.chdir(buildsettings.buildbase + "/pkg-src")
+
+buildlib.execute_shell("tar -xvf ninja-1.8.2.tar.gz")
+os.chdir("ninja-1.8.2")	
+buildlib.execute_shell("./configure.py --bootstrap")
+buildlib.execute_shell("mv ./ninja {}/bin".format(buildsettings.builddir))
